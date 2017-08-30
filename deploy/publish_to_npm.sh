73f99ce78edae6482fd3c444064cc7398408a1b4
Mikey <pmsorhaindo@gmail.com>
2017-08-30 07:29:26 +0100

    fix(npm): return non zero value if semantic release did not publish

--- deploy/publish_to_npm.sh
+++ deploy/publish_to_npm.sh
@@ -4,7 +4,7 @@ set -e # Exit with nonzero exit code if anything fails
 # We only want to publish to npm when on master
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
   echo 'testing master only deploys'
-  exit 0
+  exit 1
 fi
 
 yarn semantic-release
